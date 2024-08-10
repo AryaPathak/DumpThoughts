6f0292466204a1557ce0e61ea40b8a635e358461
Arya <pathakarya012@gmail.com>
2024-08-11 01:33:35 +0530

    DB connected

---
+++ sma-backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const userRoutes = require('./src/user/routes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.send("Hello");
+})
+
+app.use("/api/v1/users", userRoutes);
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
